0df0cbee9613e69cc581646a19804970967c0b04
abcvvo15 <abcvvo15@vt.edu>
2020-08-31 21:20:57 -0400

    update
    cmake files

--- cmake-build-debug/CMakeFiles/wys.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/wys.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/cygdrive/d/ECE1574/wys/main.cpp" "/cygdrive/d/ECE1574/wys/cmake-build-debug/CMakeFiles/wys.dir/main.cpp.o"
+  "/cygdrive/d/ECE1574/wys/alcoholConcentration/main.cpp" "/cygdrive/d/ECE1574/wys/alcoholConcentration/cmake-build-debug/CMakeFiles/wys.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /cygdrive/d/ECE1574/wys
+# Install script for directory: /cygdrive/d/ECE1574/wys/alcoholConcentration
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/cygdrive/d/ECE1574/wys/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/cygdrive/d/ECE1574/wys/alcoholConcentration/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
